15898676572c0eb3cf6b246636dff300bf031dd1
shimiharu1012 <shimizuharuki314@gmail.com>
2025-02-27 21:20:02 +0900

    Revert "Feature/section4"

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,13 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            UsersTableSeeder::class,
-            CategoriesTableSeeder::class,
-            RecipesTableSeeder::class,
-            IngredientsTableSeeder::class,
-            StepsTableSeeder::class,
-            ReviewsTableSeeder::class,
-        ]);
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
